39148d35ca4c68a2000ed34aff51480fd7329771
Wellington P.G (Wellington Geek) <welliongtongeek@gmail.com>
2016-06-25 16:51:36 -0300

    Rotas simples em node

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var app = express();
 
 var port = 5000;
 
+app.get('/', function(req, res){
+  res.send('Olá Mundo :)');
+});
+
+app.get('/livros', function(req, res){
+  res.send('Olá Livros!');
+});
+
 app.listen(5000, function(err){
   console.log('Servidor funcionando na porta -> ' + port);
 });
